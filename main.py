3c02eb2becf781d2b2324367f605534d7cda3036
André Martins <objetovazio@gmail.com>
2023-05-06 10:26:56 -0300

    init files

---
+++ main.py
@@ -0,0 +1,11 @@
+import functions as f
+
+array_img_urls = [
+  'https://s2.glbimg.com/Yu4IgOQom-psPpXjFnwfgC3DIQ0=/0x0:1381x833/924x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_bc8228b6673f488aa253bbcb03c80ec5/internal_photos/bs/2019/a/L/RhEeE8RtOxPR1Ev89EgA/barreira.jpg',
+  'https://media.gazetadopovo.com.br/haus/2016/10/sala-de-estar-edgard-corsi-anuario-haus-2016-1-1024x684-eb44e890.jpg',
+  'https://neofeed.com.br/wp-content/uploads/2019/12/Leonardo-Dicaprio-1.jpg',
+  'https://files-apichat.azureedge.net/userfiles/7145f8a61e112b7fe310108acffb7161/photos/file_461.jpg',
+  'https://media.istockphoto.com/id/470794928/pt/foto/multid%C3%A3o-de-pessoas-a-caminhar-atrav%C3%A9s-de-cidade-ocupada.jpg?s=612x612&w=is&k=20&c=E-mMq5h1HoYMHPqLj7V-MxoA4ivDbcVSyeETQVyLq6I='
+]
+
+f.get_alt_text(array_img_urls[4])
